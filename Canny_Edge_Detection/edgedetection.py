acd52645d720e053d4c8cadbcede3bb0855474cc
Ramil Sharifsoy <ramil.sharifsoy@gmail.com>
2016-11-29 14:33:45 -0600

    Run quiz - Edge Detection

--- Canny_Edge_Detection/edgedetection.py
+++ Canny_Edge_Detection/edgedetection.py
@@ -2,24 +2,25 @@
 #edges = cv2.Canny(gray, low_threshold, high_threshold)
 #As far as a ratio of low_threshold to high_threshold, John Canny himself recommended a low to high ratio of 1:2 or 1:3
 
-#doing all the relevant imports
+# Do all the relevant imports
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
 # Read in the image and convert to grayscale
-image = mpimg.imread('exit_ramp.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# Note: in the previous example we were reading a .jpg 
+# Here we read a .png and convert to 0,255 bytescale
+image = (mpimg.imread('exit_ramp.png')*255).astype('uint8')
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 # Define a kernel size for Gaussian smoothing / blurring
-kernel_size = 3
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+kernel_size = 5 # Must be an odd number (3, 5, 7...)
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
 
-# Define parameters for Canny and run it
-# NOTE: if you try running this code you might want to change these!
-low_threshold = 1
-high_threshold = 10
+# Define our parameters for Canny and run it
+low_threshold = 80
+high_threshold = 150
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
 # Display the image
