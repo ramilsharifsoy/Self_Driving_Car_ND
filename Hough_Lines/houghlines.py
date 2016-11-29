dcdd2d33000d13020e9bc5c12b151521f8e01ce1
Ramil Sharifsoy <ramilsharifsoy@users.noreply.github.com>
2016-11-29 16:01:04 -0600

    add quiz results

--- Hough_Lines/houghlines.py
+++ Hough_Lines/houghlines.py
@@ -17,14 +17,16 @@
 #in the output, and max_line_gap is the maximum distance (again, in pixels) between segments that you will 
 #allow to be connected into a single line. 
 
-# Do relevant imports
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
+
 # Read in and grayscale the image
-image = mpimg.imread('exit_ramp.jpg')
+# Note: in the previous example we were reading a .jpg 
+# Here we read a .png and convert to 0,255 bytescale
+image = (mpimg.imread('exit_ramp.png')*255).astype('uint8')
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 # Define a kernel size and apply Gaussian smoothing
@@ -36,20 +38,31 @@ low_threshold = 50
 high_threshold = 150
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
+# Next we'll create a masked edges image using cv2.fillPoly()
+mask = np.zeros_like(edges)   
+ignore_mask_color = 255   
+
+# This time we are defining a four sided polygon to mask
+imshape = image.shape
+vertices = np.array([[(0,imshape[0]),(470, 290), (480, 290), (imshape[1],imshape[0])]], dtype=np.int32)
+cv2.fillPoly(mask, vertices, ignore_mask_color)
+masked_edges = cv2.bitwise_and(edges, mask)
+
 # Define the Hough transform parameters
 # Make a blank the same size as our image to draw on
-rho = 1
-theta = np.pi/180
-threshold = 1
-min_line_length = 10
-max_line_gap = 1
-line_image = np.copy(image)*0 #creating a blank to draw lines on
+rho = 2 # distance resolution in pixels of the Hough grid
+theta = np.pi/180 # angular resolution in radians of the Hough grid
+threshold = 15     # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 50 #minimum number of pixels making up a line
+max_line_gap = 30    # maximum gap in pixels between connectable line segments
+line_image = np.copy(image)*0 # creating a blank to draw lines on
 
 # Run Hough on edge detected image
-lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+# Output "lines" is an array containing endpoints of detected line segments
+lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
                             min_line_length, max_line_gap)
 
-# Iterate over the output "lines" and draw lines on the blank
+# Iterate over the output "lines" and draw lines on a blank image
 for line in lines:
     for x1,y1,x2,y2 in line:
         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
@@ -58,6 +71,5 @@ for line in lines:
 color_edges = np.dstack((edges, edges, edges)) 
 
 # Draw the lines on the edge image
-combo = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
-plt.imshow(combo)
-
+lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
+plt.imshow(lines_edges)
